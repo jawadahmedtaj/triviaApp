55c7b460d8bb39b5a108a4e229d567e3c0650f87
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-12-24 09:15:11 +0100

    vue init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Question msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import Question from "./components/Question.vue";
+
+export default {
+  name: "App",
+  components: {
+    Question
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
