56462e36d31ad6ff11b3291f1b46a1f74cfb1479
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-12-28 15:35:43 +0100

    data format changed

--- src/store/index.js
+++ src/store/index.js
@@ -6,51 +6,50 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     count: 0,
-    MCQs: {
-      madi: [
-        {
-          question: "Question 1",
-          answers: {
-            1: "first option",
-            2: "second option"
-          },
-          answer: 1,
-          answered: false
+    MCQs: [
+      {
+        by: "Jawad",
+        question: "Question 1",
+        answers: {
+          1: "first option",
+          2: "second option"
         },
-        {
-          question: "Question 2",
-          answers: {
-            1: "first option",
-            2: "second option"
-          },
-          answer: 2,
-          answered: false
-        }
-      ]
-    },
-    binary: {
-      madi: [
-        {
-          question: "Question 1",
-          answer: 1,
-          answered: false
+        answer: 1,
+        answered: false
+      },
+      {
+        by: "Jawad",
+        question: "Question 2",
+        answers: {
+          1: "first option",
+          2: "second option"
         },
-        {
-          question: "Question 2",
-          answer: 2,
-          answered: false
-        }
-      ]
-    },
-    fillBlank: {
-      jawad: [
-        {
-          question: "Sparky get a ____",
-          answer: "life",
-          answered: false
-        }
-      ]
-    },
+        answer: 2,
+        answered: false
+      }
+    ],
+    binary: [
+      {
+        by: "Jawad",
+        question: "Question 1",
+        answer: 1,
+        answered: false
+      },
+      {
+        by: "Jawad",
+        question: "Question 2",
+        answer: 2,
+        answered: false
+      }
+    ],
+    fillBlank: [
+      {
+        by: "Jawad",
+        question: "Sparky get a ____",
+        answer: "life",
+        answered: false
+      }
+    ],
     messages: {
       maadi: "",
       jawad: "",
