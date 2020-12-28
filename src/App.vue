63fb9890b3c1e7aba975e4beae7435e234b7eab7
Jawad Ahmed <jawadahmedtaj@gmail.com>
2020-12-28 09:10:58 +0100

    components rearranged

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <v-app>
-    <v-container>
-      <v-row class="d-flex justify-center align-content-center" height="100%">
-        <v-col class="">
-          <v-slide-y-reverse-transition>
-            <div v-show="questionShow">
-              <v-btn depressed primary @click="questionDecider"> Click me </v-btn>
-            </div>
-          </v-slide-y-reverse-transition>
-        </v-col>
-      </v-row>
-      <v-row class="d-flex justify-center align-content-center" height="100%">
-        <v-col class="">
-          <v-slide-y-transition>
-            <div v-show="show"><Questions /></div>
-          </v-slide-y-transition>
-        </v-col>
-      </v-row>
+    <v-container fluid>
       <div id="app">
         <div id="nav">
           <router-link to="/">Home</router-link> |
@@ -31,69 +15,20 @@
 </template>
 
 <script>
-import Questions from "./components/Questions";
-
 export default {
   name: "App",
-  components: {
-    Questions
-  },
+  components: {},
   data: () => ({
     questionShow: true,
     show: false,
     selected: null,
-    MCQs: {
-      madi: [
-        {
-          question: "Question 1",
-          answers: {
-            1: "first option",
-            2: "second option"
-          },
-          answer: 1,
-          answered: false
-        },
-        {
-          question: "Question 2",
-          answers: {
-            1: "first option",
-            2: "second option"
-          },
-          answer: 2,
-          answered: false
-        }
-      ]
-    },
-    binary: {
-      madi: [
-        {
-          question: "Question 1",
-          answer: 1,
-          answered: false
-        },
-        {
-          question: "Question 2",
-          answer: 2,
-          answered: false
-        }
-      ]
-    },
-    fillBlank: {
-      jawad: [
-        {
-          question: "Sparky get a ____",
-          answer: "life",
-          answered: false
-        }
-      ]
-    }
   }),
   methods: {
     questionDecider() {
       this.questionShow = false;
       this.show = true;
-    }
-  }
+    },
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,51 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     count: 0,
+    MCQs: {
+      madi: [
+        {
+          question: "Question 1",
+          answers: {
+            1: "first option",
+            2: "second option"
+          },
+          answer: 1,
+          answered: false
+        },
+        {
+          question: "Question 2",
+          answers: {
+            1: "first option",
+            2: "second option"
+          },
+          answer: 2,
+          answered: false
+        }
+      ]
+    }, 
+    binary: {
+      madi: [
+        {
+          question: "Question 1",
+          answer: 1,
+          answered: false
+        },
+        {
+          question: "Question 2",
+          answer: 2,
+          answered: false
+        }
+      ]
+    },
+    fillBlank: {
+      jawad: [
+        {
+          question: "Sparky get a ____",
+          answer: "life",
+          answered: false
+        }
+      ]
+    }
   },
   mutations: {
     increment(state) {
